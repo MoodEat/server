8c9410431c0f52138f9e18dd63b5aab02367f5cf
hikmanisyariful <syarifulfajar017@gmail.com>
2020-04-20 11:10:09 +0700

    mongoDb atlas

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ const cors = require('cors')
 
 /* local */ 
 
-const {dbName, client} = require('./config/config')
+// const {dbName, client} = require('./config/config')
 
 /* atlas */
 
-// const { MongoClient } = require('mongodb')
-// const db_name = process.env.DB_NAME
-// const user = process.env.DB_USER
-// const password = process.env.DB_PASSWORD
-// const uri = `mongodb+srv://${user}:${password}@cluster0-gjr7b.mongodb.net/${db_name}?retryWrites=true&w=majority`
+const { MongoClient } = require('mongodb')
+const db_name = process.env.DB_NAME
+const user = process.env.DB_USER
+const password = process.env.DB_PASSWORD
+const uri = `mongodb+srv://${user}:${password}@cluster0-gjr7b.mongodb.net/${db_name}?retryWrites=true&w=majority`
 
 app.use(cors())
 app.use(morgan('tiny'))
@@ -27,39 +27,39 @@ app.use(express.json())
 
 /* local */ 
 
-client.connect(err => {
-  if (err) console.log(err)
-  else console.log('connct mongodb successfully')
+// client.connect(err => {
+//   if (err) console.log(err)
+//   else console.log('connct mongodb successfully')
 
-  const db = client.db(dbName)
+//   const db = client.db(dbName)
 
-  app.use((req, res, next) => {
-    req.db = db
-    next()
-  })
-
-  app.use(routes)
-  app.use(errorHandler)
-})
-
-/* atlas */
-
-// MongoClient.connect(uri, {
-//   useUnifiedTopology: true
-// })
-// .then( client => {
-//     console.log('connected to database')
-//     const db = client.db(db_name)
-//     app.use((req, res, next) => {
-//       req.db = db
-//       next()
+//   app.use((req, res, next) => {
+//     req.db = db
+//     next()
 //   })
+
 //   app.use(routes)
 //   app.use(errorHandler)
 // })
-// .catch(error => {
-//     console.error(error)
-//   })
+
+/* atlas */
+
+MongoClient.connect(uri, {
+  useUnifiedTopology: true
+})
+.then( client => {
+    console.log('connected to database')
+    const db = client.db(db_name)
+    app.use((req, res, next) => {
+      req.db = db
+      next()
+  })
+  app.use(routes)
+  app.use(errorHandler)
+})
+.catch(error => {
+    console.error(error)
+  })
 
 module.exports = app
 
